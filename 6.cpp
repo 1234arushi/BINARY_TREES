9e7ff79ac1659771a9090b23008b338b13a8d115
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-11-22 22:32:58 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,6 +1,5 @@
 //https://leetcode.com/problems/binary-tree-preorder-traversal/submissions/847568692/
 
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -14,18 +13,42 @@
  */
 class Solution {
 public:
-    vector<int> arr;
+    //vector<int> arr;-->method 1:
     vector<int> preorderTraversal(TreeNode* root) {
 
-        
+        //method 2:iterative approach
+        vector<int> preorder;
         if(root==NULL)
+        {
+            return preorder;
+        }
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty())
+        {
+            root=st.top();
+            st.pop();//LIFO
+            preorder.push_back(root->val);
+            if(root->right!=NULL)
+            {
+                st.push(root->right);
+            }
+            if(root->left!=NULL)
+            {
+                st.push(root->left);
+            }
+        }
+        return preorder;
+
+        //method 1:recursive
+        /*if(root==NULL)
         {
             return arr;
         }
         arr.push_back(root->val);
         preorderTraversal(root->left);
         preorderTraversal(root->right);
-        return arr;
+        return arr;*/
         
     }
 };
